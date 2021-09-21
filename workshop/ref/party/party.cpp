bd8f1550394ba17f04c33508f6730ebbe6c296d1
mika mikaela <luunamdat204@gmail.com>
2021-09-22 06:58:15 +0700

    removed the old cringe code of ref/party

---
+++ workshop/ref/party/party.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+#define pi pair<int, int>
+#define st first
+#define nd second
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	return 0;
+}
