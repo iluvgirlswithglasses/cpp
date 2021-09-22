3da0bc8259841f76e5584fcf0abec7dcbc1c6227
mika mikaela <luunamdat204@gmail.com>
2021-09-22 07:27:41 +0700

    ref/party update

--- workshop/ref/party/party.cpp
+++ workshop/ref/party/party.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 #define pi pair<int, int>
 #define st first
 #define nd second
+const int N = 1e5+7;
+const int R = 1e9+7;
+
+int n, w[N];
+vector<int> adj[N];
+
+pi dfs(int u, int p) {
+	pi cr = {w[u], 0};	// {choose u, discard u}
+	for (int &v: adj[u]) if (v != p) {
+		pi child = dfs(v, u);
+		cr.st += child.nd;	// choose this, discard child
+		cr.nd += max(child.st, child.nd);	// discard this, choose or discard child
+	}
+	return cr;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(0);
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> w[i];
+	for (int i = 0, u, v; i < n-1; i++) {
+		cin >> u >> v; u--, v--;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	cout << dfs(0, -1).st << "\n";
 	return 0;
 }
