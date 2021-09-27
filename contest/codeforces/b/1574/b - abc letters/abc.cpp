a6b23fef89c894ea2fad8fac9d5c7692b0b64189
iluvgirlswithglasses <luunamdat204@gmail.com>
2021-09-27 10:40:02 +0700

    codeforces: abc (what an easy problem)

--- contest/codeforces/b/1574/b - abc letters/abc.cpp
+++ contest/codeforces/b/1574/b - abc letters/abc.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-#define u64 unsigned long long
-u64 a, b, c, m;
+#define ll long long
+
+ll max(ll a, ll b) {
+	if (a > b) return a;
+	return b;
+}
+
+ll a, b, c, m;
 
 bool calc() {
-
-	return 0;
+	ll maxm = max(0, a-1) + max(0, b-1) + max(0, c-1);
+	ll minm[3] = {a-b-c-1, b-a-c-1, c-a-b-1};
+	return *max_element(minm, minm+3) <= m &&
+		   m <= maxm;
 }
 
 int main() {
