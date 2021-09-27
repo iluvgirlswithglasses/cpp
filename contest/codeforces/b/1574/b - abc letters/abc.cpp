c18acc645d0407a3f43e3b774f24f133d7214cc8
mika mikaela <luunamdat204@gmail.com>
2021-09-27 16:12:41 +0700

    codeforces: abc letter - setup commit

---
+++ contest/codeforces/b/1574/b - abc letters/abc.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+#define u64 unsigned long long
+u64 a, b, c, m;
+
+bool calc() {
+
+	return 0;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	int t; cin >> t;
+	while (t--) {
+		cin >> a >> b >> c >> m;
+		if (calc()) {
+			cout << "YES\n";
+		} else {
+			cout << "NO\n";
+		}
+	}
+	return 0;
+}
