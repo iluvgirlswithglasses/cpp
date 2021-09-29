b6cd4be267459bf33ef7df12d27c1b3f909c6108
mika mikaela <luunamdat204@gmail.com>
2021-09-29 22:53:35 +0700

    codeforces: book / setup and go to sleep

--- contest/codeforces/b/1573/c - book/book.cpp
+++ contest/codeforces/b/1573/c - book/book.cpp
@@ -1,41 +1,35 @@
-
-// change the bracket style...
-
 #include <iostream>
-#include <cstring>
 #include <vector>
 using namespace std;
 
-const int N = 2e5+7;
-int n, k[N];
-vector<int> dep[N];	// dependencies :)
+#define pi pair<int, int>
+#define st first
+#define nd second
 
-int calc()
-// calc <= n all cases
-// -1 happens if deadlock presents
-{
-	if (check_deadlock())
-		return -1;
-	return _calc();
+const int N = 2e5+7;
+int n;
+vector<pi> adj[N];
+
+int calc() {
+	int res = -1;
+	return 1 + res;
 }
 
-int main() 
-{
+int main() {
 	ios_base::sync_with_stdio(false); cin.tie(0);
 	int t; cin >> t;
-	while (t--) 
-	{
+	while (t--) {
 		cin >> n;
-		memset(k, 0, n*sizeof(k[0]));
 		//
-		for (int i = 0; i < n; i++) 
-		{
-			cin >> k[i];
-			dep[i].resize(k[i]);
-			for (int j = 0; j < k[i]; j++) 
-				cin >> dep[i][j];
+		for (int u = 0, k; u < n; u++) {
+			cin >> k;
+			adj[u].resize(k);
+			//
+			for (int i = 0, v; i < k; i++) {
+				cin >> v;
+				adj[u][i] = {v, u > v ? 0: 1};
+			}
 		}
-		//
 		cout << calc() << "\n";
 	}
 	return 0;
