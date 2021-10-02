911a64499c580477db29c083c8bcc6fe182bd8b3
mika mikaela <luunamdat204@gmail.com>
2021-10-02 09:54:14 +0700

    codeforces 1567: d - base11 / update

--- contest/codeforces/b210908/1567/d - base11/base11.cpp
+++ contest/codeforces/b210908/1567/d - base11/base11.cpp
@@ -4,19 +4,18 @@ using namespace std;
 
 int s, n;
 
-void calc() {
-	vector<int> f(n, 0);
+void calc(vector<int> &f) {
 	int i = 0;
 	for (int e = 1e9; e > 0; e /= 10) {
 		while (s >= e && s-e >= n-(i+1)) {	// n-(i+1): unfilled count
-			f[i] += e;
+			f[i++] += e;
 			s -= e;
-			if (i < n-1) i++; 
+			if (i == n) {
+				f[n-1] += s;
+				return;
+			}
 		}
 	}
-	//
-	for (int &i: f) cout << i << " ";
-	cout << "\n";
 }
 
 int main() {
@@ -26,8 +25,13 @@ int main() {
 		cin >> s >> n;
 		if (n == 1)
 			cout << s << "\n";
-		else
-			calc();
+		else {
+			vector<int> f(n, 0);
+			calc(f);
+			//
+			for (int &i: f) cout << i << " ";
+			cout << "\n";
+		}
 	}
 	return 0;
 }
