c7ded5d600fe2e5d7a9c0a5e293566f54c0cf579
iluvgirlswithglasses <luunamdat204@gmail.com>
2021-12-05 00:44:18 +0700

    the most stupid statement I've read in years

---
+++ workshop/class/iluvgirlswithglasses/211204/supersch-test.sh
@@ -0,0 +1,9 @@
+# this is multiple answer question
+# yet, we do not have a checker
+# so,... judge your work by the FIRST NUMBER only
+
+b supersch.cpp "./supersch-test/test$1.inp";
+echo "";
+cat "./supersch-test/test$1.out";
+echo "";
+
--- workshop/class/iluvgirlswithglasses/211204/supersch.cpp
+++ workshop/class/iluvgirlswithglasses/211204/supersch.cpp
@@ -14,11 +14,18 @@ BTW I use Arch
 */
 
 /*
-done: 12:01pm (22mins)
+đề không hề ghi rằng:
+	khi có nhiều người về đích cùng lúc, tất cả mọi người sẽ đều được phát huy chương
+và trong khi đề cho constraint N <= 1000,
+test 6 lại cho N = 1063
+
+bruh hết sức bruh
 */
 
 #include <iostream>
-#include <set>
+#include <map>
+#include <vector>
+#include <cstdio>
 using namespace std;
 
 #define pi pair<int, int>
@@ -28,7 +35,7 @@ using namespace std;
 
 const int N = 1e3+7, K = 102;
 int n, k, a[N][K];
-set<pi> s;
+map<int, vector<int>> resource_holder;
 
 int org[K], wincnt;
 
@@ -39,12 +46,12 @@ int main() {
 	//
 	cin >> n >> k;
 	n--;
-	for (int i = 0; i < n; i++)
-	for (int j = 0; j < k; j++)
-		cin >> a[i][j];
+	for (int j = 0; j < n; j++)
+	for (int i = 0; i < k; i++)
+		cin >> a[j][i];
 	for (int i = 0; i < k; i++) {
 		int x; cin >> x;
-		s.insert(mkpair(x, i+1));
+		resource_holder[x].push_back(i+1);
 	}
 	//
 	for (int i = 0; i < k; i++) {
@@ -52,12 +59,15 @@ int main() {
 		for (int j = 1; j < n; j++)
 			mi = min(mi, a[j][i]);
 		//
-		set<pi>::iterator p = s.lower_bound(mkpair(mi, 0));
-		if (p != s.begin()) {
+		map<int, vector<int>>::iterator p = resource_holder.upper_bound(mi);
+		if (p != resource_holder.begin()) {
 			p--;
 			wincnt++;
-			org[i] = p->nd;
-			s.erase(p);
+			org[i] = p->nd.back();
+			p->nd.pop_back();
+			if (p->nd.size() == 0) {
+				resource_holder.erase(p);
+			}
 		}
 	}
 	//
@@ -66,8 +76,12 @@ int main() {
 		if (org[i] > 0) {
 			cout << org[i] << " ";
 		} else {
-			cout << s.begin()->nd << " ";
-			s.erase(s.begin());
+			map<int, vector<int>>::iterator p = resource_holder.begin();
+			cout << p->nd.back() << " ";
+			p->nd.pop_back();
+			if (p->nd.size() == 0) {
+				resource_holder.erase(p);
+			}
 		}
 	}
 	cout << "\n";
