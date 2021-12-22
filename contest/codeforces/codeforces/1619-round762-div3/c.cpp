4243be5fcb65bdca1d2eabd17801d396ab5f6a1b
iluvgirlswithglasses <luunamdat204@gmail.com>
2021-12-22 10:02:15 +0700

    nightmarish code

--- contest/codeforces/codeforces/1619-round762-div3/c.cpp
+++ contest/codeforces/codeforces/1619-round762-div3/c.cpp
@@ -16,31 +16,37 @@ BTW I use Arch
 #include <iostream>
 #include <string>
 #include <vector>
-#include <set>
-#include <algorithm>
 using namespace std;
 
-string merge(const vector<int>& b, int l) {
+string merge(const vector<int>& b, int r) {
 	string s;
-	for (int i = l; i < b.size(); i++) 
+	for (int i = r; i >= 0; i--)
 		s += to_string(b[i]);
 	return s;
 }
 
 string calc(const string& a, const string& s) {
-	// có bao nhiêu chỗ để tạo tổng có giá trị >= 10
-	vector<bool> loc(s.size());
-	for (int i = 0; i < s.length()-1; i++) {
-		if (s[i] == '1') loc[i] = true;
-	}
-	//
-	for (int mk = 0; mk < (1<<loc.size()); mk++) {
-		vector<int> b(s.size(), 0);
+	vector<int> b(s.size(), 0);
+	for (int in_a = a.size()-1, in_s = s.size()-1, in_b = 0; in_a >= 0 || in_s >= 0; in_a--, in_s--) {
+		if (in_s == 0 && in_a > 0) return "-1";
 		//
-		for (int in_a = a.size()-1, in_s = s.size() - 1; ; in_a--, in_b--) {
-
+		int cra = in_a >= 0 ? (a[in_a] - '0') : 0;
+		if (s[in_s] - '0' < cra) {
+			if (in_s-- == 0 || s[in_s] != '1') return "-1";
+			int target = stoi(s.substr(in_s, 2));
+			//
+			if (target < 19)
+				b[in_b++] = target - cra;
+			else
+				return "-1";
+		} else {
+			b[in_b++] = (s[in_s] - '0') - cra;
 		}
 	}
+	//
+	for (int i = b.size()-1; i >= 0; i--)
+		if (b[i] != 0) return merge(b, i);
+	return "0";
 }
 
 int main() {
