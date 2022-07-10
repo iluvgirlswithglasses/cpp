4d378dbf1767b4fba43c0916aa1a482d1375a5b6
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-07-11 00:03:20 +0700

    alright imma sleep

--- contest/freecontest/testing-round/tr33/incpos.cpp
+++ contest/freecontest/testing-round/tr33/incpos.cpp
@@ -24,9 +24,39 @@ then print the number of this occurrences: `a[i] > a[i+1]`
 using namespace std;
 
 const int N = 2e5+7;
-int n, f[N<<1];
+// f[i]:	number of occurrences managed by node `f[i]`
+// fl[i]:	the value of the left-most element managed by `f[i]`
+// fr[i]:	same of `fl[i]` for right-most
+// fd[i]:	lazy update for `fl[]` and `fr[]`
+int n, q, f[N<<1], fl[N<<1], fr[N<<1], fd[N];
+
+void init() {
+	for (int i = n-1; i > 0; i--) {
+		int l = i<<1, r = i<<1|1;
+		f[i] = f[l] + f[r];
+		fl[i] = fl[l]; fr[i] = fr[r];
+		// found one more occurrences when joining two child nodes
+		if (fr[l] > fl[r]) f[i]++;
+	}
+}
+
+void upd(int l, int r, int v) {
+
+}
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(0);
+	cin >> n >> q;
+	for (int i = n; i < (n<<1); i++) {
+		int x; cin >> x;
+		fl[i] = fr[i] = x;
+	}
+	init();
+	//
+	while (q--) {
+		int l, r, v; cin >> l >> r >> v;
+		upd(l-1, r, v);
+		cout << f[1] << "\n";
+	}
 	return 0;
 }
