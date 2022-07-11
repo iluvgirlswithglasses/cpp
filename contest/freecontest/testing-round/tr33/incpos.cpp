14a9c43f638e42a20bf1da081ffebf498d130cae
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-07-11 18:10:14 +0700

    i got a good sleep

--- contest/freecontest/testing-round/tr33/incpos.cpp
+++ contest/freecontest/testing-round/tr33/incpos.cpp
@@ -23,40 +23,59 @@ then print the number of this occurrences: `a[i] > a[i+1]`
 #include <iostream>
 using namespace std;
 
+#define ll long long
+
 const int N = 2e5+7;
-// f[i]:	number of occurrences managed by node `f[i]`
-// fl[i]:	the value of the left-most element managed by `f[i]`
-// fr[i]:	same of `fl[i]` for right-most
-// fd[i]:	lazy update for `fl[]` and `fr[]`
-int n, q, f[N<<1], fl[N<<1], fr[N<<1], fd[N];
+// cnt: count of occurrences
+int	n, q, cnt;
+// f[]: simple upd-N-qry-1 segtree
+ll f[N<<1];
+// pos[i]: returns true if occurs `a[i] > a[i+1]`
+bool pos[N];
 
-void init() {
-	for (int i = n-1; i > 0; i--) {
-		int l = i<<1, r = i<<1|1;
-		f[i] = f[l] + f[r];
-		fl[i] = fl[l]; fr[i] = fr[r];
-		// found one more occurrences when joining two child nodes
-		if (fr[l] > fl[r]) f[i]++;
+struct segtree {
+
+	void upd(int l, int r, ll v) {
+		for (l+=n, r+=n; l < r; l>>=1, r>>=1) {
+			if (l&1) f[l++] += v;
+			if (r&1) f[--r] += v;
+		}
 	}
-}
 
-void upd(int l, int r, int v) {
+	ll get(int p) {
+		ll r = 0;
+		for (p+=n; p > 0; p>>=1)
+			r += f[p];
+		return r;
+	}
+} st;
 
+void check(int i) {
+	if (st.get(i) > st.get(i+1) && !pos[i]) {
+		cnt++;
+		pos[i] = true;
+	} else if (st.get(i) <= st.get(i+1) && pos[i]) {
+		cnt--;
+		pos[i] = false;
+	}
 }
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(0);
 	cin >> n >> q;
-	for (int i = n; i < (n<<1); i++) {
-		int x; cin >> x;
-		fl[i] = fr[i] = x;
+	for (int i = 0; i < n; i++) 
+		cin >> f[i+n];
+	for (int i = 0; i < n-1; i++) if (f[n+i] > f[n+i+1]) {
+		cnt++;
+		pos[i] = true;
 	}
-	init();
 	//
 	while (q--) {
 		int l, r, v; cin >> l >> r >> v;
-		upd(l-1, r, v);
-		cout << f[1] << "\n";
+		st.upd(--l, r--, v);
+		if (l > 0) check(l-1);	// left
+		if (r < n-1) check(r);	// right
+		cout << cnt << "\n";
 	}
 	return 0;
 }
