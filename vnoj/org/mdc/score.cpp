d6d1f820416dcc366b36c29d0cf77da3e7f4137e
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-09-29 22:30:57 +0700

    funny problem by Newplayers12

---
+++ vnoj/org/mdc/score.cpp
@@ -0,0 +1,26 @@
+
+/*
+author: 	iluvgirlswithglasses 
+github: 	https://github.com/iluvgirlswithglasses 
+created:	Thu Sep 29 22:29:32 2022
+tab-width:	4 spaces
+
+ /\_/\
+( o.o )
+ > ^ <
+
+I sexually identify myself as an Attack Helicopter
+BTW I use Arch
+*/
+
+#include <cstdio>
+using namespace std;
+
+#define ll long long
+ll a, b;
+
+int main() {
+	scanf("%lld %lld", &a, &b);
+	printf("%lld\n", (a|b));
+	return 0;
+}
