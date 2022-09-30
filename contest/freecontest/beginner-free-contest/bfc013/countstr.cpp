246d739cc31c29d5e7c095a61da694cca22c2b91
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-09-30 22:53:07 +0700

    my favorite solution for this embarrassing incident

--- contest/freecontest/beginner-free-contest/bfc013/countstr.cpp
+++ contest/freecontest/beginner-free-contest/bfc013/countstr.cpp
@@ -1,7 +1,7 @@
 /*
 author: 	iluvgirlswithglasses 
 github: 	https://github.com/iluvgirlswithglasses 
-created:	Fri Sep 30 20:55:42 2022
+created:	Thu Sep 29 14:14:22 2022
 tab-width:	4 spaces
 
  /\_/\
@@ -14,46 +14,63 @@ BTW I use Arch
 
 #include <iostream>
 #include <string>
-#include <cstring>
 using namespace std;
 
+#define all(c) c.begin(), c.end()
 #define ll long long
+#define pi pair<int, int>
+#define st first
+#define nd second
 
 const int N = 1e5+7;
-int n, a[N], dp[26][N];
+int n, a[N], last[26];
 
-void read() {
-	memset(dp, -1, sizeof(dp));
-	string s; cin >> s;
-	n = s.length();
-	for (int i = 0; i < n; i++)
-		a[i] = s[i] - 'a';
+// f[c][i] == how many char `c` in node `i`
+int f[26][N<<1];
+
+int query(int c, int l, int r) {
+	int res = 0;
+	for (l+=n, r+=n; l < r; l>>=1, r>>=1) {
+		if (l&1) res += f[c][l++];
+		if (r&1) res += f[c][--r];
+	}
+	return res;
 }
 
-void gen() {
-	for (int c = 0; c < 26; c++)
+void read() {
+	string s; cin >> s;
+	n = s.length();
 	for (int i = 0; i < n; i++) {
-		if (c == a[i])
-			dp[c][i] = i;
-		else
-			dp[c][i] = i > 0 ? dp[c][i-1] : -1;
+		a[i] = s[i] - 'a';
+		f[a[i]][n+i] = 1;
 	}
+	for (int c = 0; c < 26; c++)
+	for (int i = n-1; i >= 1; i--) {
+		f[c][i] = f[c][i<<1] + f[c][i<<1|1];
+	}
+}
+
+ll calc() {
+	ll res = 0;
+	last[a[0]] = 1;
+	for (int r = 1; r < n; r++) {
+		int l = last[a[r]];
+		//
+		for (int c = 0; c < 26; c++) {
+			// if there > 1 `c` in this seg
+			// only 1 `c` can make s[c:r]
+			res += min(query(c, l, r), 1);
+		}
+		//
+		last[a[r]] = r+1;
+	}
+	return res;
 }
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(0);
 	read();
-	gen();
-	ll res = 0;
-	for (int c = 0; c < 26; c++)
-	for (int i = n-1; i >= 0; i--) 
-	if (c != a[i] && dp[c][i] != -1) {
-		int left = dp[a[i]][i-1];
-		if (left == -1)
-			res++;
-		else
-			res += left < dp[c][i];
-	}
-	cout << res << "\n";
+	cout << calc() << "\n";
 	return 0;
 }
+
