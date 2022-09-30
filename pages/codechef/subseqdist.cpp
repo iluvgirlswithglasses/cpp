fea690486d44021190b8ee78f48f4ffe5c7dd9f8
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-09-30 17:08:51 +0700

    explaination added

--- pages/codechef/subseqdist.cpp
+++ pages/codechef/subseqdist.cpp
@@ -19,6 +19,31 @@ BTW I use Arch
 #include <cmath>
 using namespace std;
 
+/*
+mấu chốt thứ nhất:
+	trong lúc biến đổi phần tử xuất hiện nhiều lần nhất,
+	chúng ta có thể rảnh tay thay đổi các phần tử còn lại
+
+	do đó, bài toán suy biến thành:
+		cho một dãy `m` số giống nhau, 
+		hỏi tốn bao nhiêu bước để dãy trở thành `m` số riêng biệt
+
+mấu chốt thứ 2:
+	giả sử ta có `4` bước
+
+	số cách áp dụng `4` bước này lên một số bất kỳ trong dãy 
+	được biểu hiện bằng bitmask có 4 bit, ví dụ: 
+		0001 (chỉ áp dụng bước 1)
+		0101 (áp dụng bước 1 & 3)
+		1111 (áp dụng cả 4 bước)
+
+	mà các số trong dãy đều bằng nhau,
+	do đó, với `4` bước có thể tạo ra tối đa `2^4` số riêng biệt
+
+--> đáp án của bài toán sẽ là: 
+    ceil(log2(số lần xuất hiện của số xuất hiện nhiều nhất))
+*/
+
 #define all(c) c.begin(), c.end()
 #define ll long long
 #define pi pair<const int, int>
