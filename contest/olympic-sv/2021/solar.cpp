112625913f1f5c79fb8349ef37926a0c02590f39
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-10-03 17:48:13 +0700

    finally

--- contest/olympic-sv/2021/solar.cpp
+++ contest/olympic-sv/2021/solar.cpp
@@ -52,21 +52,11 @@ struct SumSegtree {
 } sst;
 
 struct MinSegtree {
-	struct CmpObj {
-		// compare object
-		int v, i, cr_index;
-
-		bool operator < (const CmpObj &x) const {
-			if (v == x.v)
-				return cr_index < x.cr_index;
-			return v < x.v;
-		}
-	};
-
 	pi f[N<<1];
 
 	void init() {
 		for (int i = 0; i < n; i++)
+			// current position of `i` panel: (i + shift) % n
 			f[i+n] = {a[i], i};
 		for (int i = n-1; i > 0; i--)
 			f[i] = min(f[i<<1], f[i<<1|1]);
@@ -78,27 +68,11 @@ struct MinSegtree {
 			f[i] = min(f[i<<1], f[i<<1|1]);
 	}
 
-	CmpObj mkobj(int i) {
-		CmpObj x;
-		x.v = f[i].st; 
-		x.i = f[i].nd;
-		x.cr_index = (f[i].nd + shift) % n;
-		return x;
-	}
-
-	CmpObj get(int l, int r) {
-		/*
-		the pb is here:
-			I compared updated positions of every accessed nodes in the following loop
-			but I did not compare the updated positions of those unvisited nodes
-
-			That's why I still got WA
-			I'll solve this later
-		*/
-		CmpObj res = {I, I, I};
+	pi get(int l, int r) {
+		pi res = {I, I};
 		for (l+=n, r+=n; l < r; l>>=1, r>>=1) {
-			if (l&1) res = min(res, mkobj(l++));
-			if (r&1) res = min(res, mkobj(--r));
+			if (l&1) res = min(res, f[l++]);
+			if (r&1) res = min(res, f[--r]);
 		}
 		return res;
 	}
@@ -116,21 +90,66 @@ int main() {
 		switch (t) {
 			case 1: {
 				int d; cin >> d;
-				shift += d;
-				if (shift >= n) shift -= n;
+				shift = (shift + d) % n;
 				break;
 			}
 			case 2: {
+				int at_zero = (n - shift) % n;	// the panel with this id is currently at position 0
 				int s, e, p; cin >> s >> e >> p;
 				s = (s-1 - shift + n) % n;
 				e = (e-1 - shift + n) % n;
-				MinSegtree::CmpObj replaced;
-				if (s <= e)
-					replaced = mst.get(s, e+1);
-				else
-					replaced = min(mst.get(s, n), mst.get(0, e+1));
-				mst.upd(replaced.i, p);
-				sst.upd(replaced.i, p);
+				pi replaced;
+				if (s <= e) {
+					// if zero is between of the query
+					if (s <= at_zero && at_zero <= e) {
+						// the current position of `lef` is always bigger than `rig`
+						pi lef = mst.get(at_zero, e+1);
+						pi rig = mst.get(s, at_zero);
+						// if value is equal --> choose the one on the left (the one which has lower position)
+						if (lef.st == rig.st)
+							replaced = lef;
+						else
+							replaced = min(lef, rig);
+
+					} else {
+						replaced = mst.get(s, e+1);
+					}
+				} else {
+					if (e < at_zero && at_zero < s) {
+						// zero is at nowhere
+						pi lef = mst.get(s, n);
+						pi rig = mst.get(0, e+1);
+						//
+						if (lef.st == rig.st)
+							replaced = lef;
+						else
+							replaced = min(lef, rig);
+
+					} else if (s <= at_zero && at_zero < n) {
+						// zero is in [s:n)
+						pi lef = mst.get(at_zero, n);
+						pi rig = min(mst.get(0, e+1), mst.get(s, at_zero));
+
+						//
+						if (lef.st == rig.st)
+							replaced = lef;
+						else
+							replaced = min(lef, rig);
+
+					} else if (0 <= at_zero && at_zero <= e) {
+						// zero is in [0:e]
+						pi lef = min(mst.get(at_zero, e+1), mst.get(s, n));
+						pi rig = mst.get(0, at_zero);
+						//
+						if (lef.st == rig.st)
+							replaced = lef;
+						else
+							replaced = min(lef, rig);
+
+					}
+				}
+				mst.upd(replaced.nd, p);
+				sst.upd(replaced.nd, p);
 				break;
 			}
 			case 3: {
