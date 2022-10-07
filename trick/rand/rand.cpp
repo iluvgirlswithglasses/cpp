673beb0ab0a07831cea6fb578680b362d1704f8c
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-10-07 09:36:46 +0700

    updated random tools

--- trick/rand/rand.cpp
+++ trick/rand/rand.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ull unsigned long long
 
-unsigned long long llrand() {
-    unsigned long long r = 0;
-
-    for (int i = 0; i < 5; ++i) {
+ull llrand_0() {
+    ull r = 0;
+    for (int i = 0; i < 5; i++) {
         r = (r << 15) | (rand() & 0x7FFF);
     }
     // write as many more F, like 0xFFFFFFFFFFFFFFFFFFULL ...
@@ -13,10 +13,17 @@ unsigned long long llrand() {
     return r & 0xFFFFFFFFFULL;
 }
 
+ull llrand_1() {
+    ull r = 0;
+    // each bit has 50% chance to be turned on
+    for (int i = 0; i < 64; i++)
+        if (rand() % 2) r |= 1<<i;
+    return r;
+}
+
 int main() {
-	srand((unsigned) time(0));
-	cout << llrand();
-	//
-	getchar();
+	srand(chrono::system_clock::now().time_since_epoch().count());
+    ull n; cin >> n;
+	cout << (llrand_1() % n) << "\n";
 	return 0;
 }
