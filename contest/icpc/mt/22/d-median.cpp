f619f1b24b3cf02b71ba57e708ee3ce00f734613
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-11-09 19:30:57 +0700

    write down some references

---
+++ contest/icpc/mt/22/d-median.cpp
@@ -0,0 +1,36 @@
+
+/*
+author: 	iluvgirlswithglasses 
+github: 	https://github.com/iluvgirlswithglasses 
+created:	Wed Nov 09 19:29:30 2022
+tab-width:	4 spaces
+
+ /\_/\
+( o.o )
+ > ^ <
+
+I sexually identify myself as an Attack Helicopter
+BTW I use Arch
+*/
+
+#include <iostream>
+using namespace std;
+
+/*
+references:
+	https://codeforces.com/blog/entry/46503
+	https://stackoverflow.com/questions/26296624/order-statistic-on-intervals
+	https://blog.anudeep2011.com/persistent-segment-trees-explained-with-spoj-problems/
+	https://www.spoj.com/problems/MKTHNUM/
+*/
+
+typedef long long ll;
+typedef pair<int, int> pi;
+#define st first
+#define nd second
+#define all(c) c.begin(), c.end()
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	return 0;
+}
