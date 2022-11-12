65a76418b94bfebaa6099a81e2a4667160b53dd7
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-11-12 10:29:53 +0700

    ok imma clean my keyboard first

---
+++ contest/bedao/mini-contest/14/operation.cpp
@@ -0,0 +1,54 @@
+
+/*
+author: 	iluvgirlswithglasses 
+github: 	https://github.com/iluvgirlswithglasses 
+created:	Sat Nov 12 10:25:22 2022
+tab-width:	4 spaces
+
+ /\_/\
+( o.o )
+ > ^ <
+
+I sexually identify myself as an Attack Helicopter
+BTW I use Arch
+*/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+#define st first
+#define nd second
+#define all(c) c.begin(), c.end()
+
+const int N = 1e5+7;
+int n, q, a[N];
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	cin >> n >> q;
+	if (n == 1) {
+		int a; cin >> a;
+		while (q--) {
+			int x; cin >> x;
+			if (x == a)
+				cout << "Yes\n";
+			else
+				cout << "No\n";
+		}
+	} else {
+		for (int i = 0; i < n; i++)
+			cin >> a[i];
+		int l = *min_element(a, a+n), r = *max_element(a, a+n);
+		while (q--) {
+			int x; cin >> x;
+			if (l <= x && x <= r)
+				cout << "Yes\n";
+			else
+				cout << "No\n";
+		}
+	}
+	return 0;
+}
