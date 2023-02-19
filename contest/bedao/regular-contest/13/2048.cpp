0701c069e2b58b7ecde4b6b09d8ec1adb9b8b8cf
iluvgirlswithglasses <luunamdat204@gmail.com>
2023-02-19 11:51:03 +0700

    mle

--- contest/bedao/regular-contest/13/2048.cpp
+++ contest/bedao/regular-contest/13/2048.cpp
@@ -13,33 +13,81 @@ I firmly believe in the supremacy of the Euphonium
 BTW I use Arch
 */
 
-// basically 1e12 % 2048 == 0
+// basically 1e11 % 2048 == 0
 // calculate 10^x / 2046 into a / b and you'll notice the pattern of `b`
 
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 typedef long long ll;
-typedef pair<int, int> pi;
+typedef pair<int, char> pi;
 #define st first
 #define nd second
 #define all(c) c.begin(), c.end()
 
-const ll R = 1e9+9;		// prime number
+const ll R = 1e9+9, N = 48828125;
 
-string s, t;
+ll     int_tb[N];
+string str_tb[N];
 
-ll head_check(string s) {
-	return 0ll;
+void gen_tb() {
+	for (ll i = 0; i < N; i++) {
+		int_tb[i] = i * 2048ll;
+
+		string str = to_string(int_tb[i]);
+		str_tb[i] = string(11 - str.length(), '0') + str;
+	}
 }
 
-ll tail_check(string t) {
-	return 0ll;
+/**
+ * @ utils
+ * */
+ll get_max(string &t) {
+	string ans = string(t.length(), '9');
+	for (int i = 0; i < t.length(); i++)
+		if (t[i] != '?') ans[i] = t[i];
+	return stoll(ans);
 }
 
+vector<pi> get_pos(string &t) {
+	vector<pi> res;
+	for (int i = 0; i < t.length(); i++)
+		if (t[i] != '?') res.push_back({i, t[i]});
+	return res;
+}
+
+bool check(string &s, vector<pi> &pos) {
+	for (pi &p: pos)
+		if (s[p.st] != p.nd) return false;
+	return true;
+}
+
+ll head_check(string &s) {
+	ll ans = 1ll;
+	for (char c: s)
+		if (c == '?') ans = ans * 10 % R;
+	return ans;
+}
+
+ll tail_check(string &t) {
+	ll cnt = 0;
+	ll mx  = get_max(t);
+	vector<pi> pos = get_pos(t);
+	for (int i = 0; i < N && int_tb[i] <= mx; i++)
+		if (check(str_tb[i], pos)) cnt++;
+	return cnt;
+}
+
+/**
+ * @ drivers
+ * */
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(0);
+	gen_tb();
+	//
+	string s, t;
 	cin >> s;
 	if (s.length() <= 12) {
 		cout << tail_check(s) << "\n";
